5b055d918c6bf27e37428b6faeee4e18214dba80
Oscar Kruithof <oscar@kruithof.io>
2024-11-19 20:46:04 +0100

    Feat: added solvability tests

--- integration.zsh
+++ integration.zsh
@@ -17,7 +17,7 @@ done
 for file in $(ls ./examples/unsolvable/*);
 do
 	printf "%-40s " $file
-	RESULT=$(./target/release/n-puzzle $command $file > /dev/null 2> /dev/null)
+	RESULT=$(./target/release/n-puzzle $command --dry $file > /dev/null 2> /dev/null)
 	if [ $? -eq 1 ]
 	then
 		echo "\t[OK]"
@@ -30,6 +30,8 @@ for command in $COMMANDS;
 do
 	for file in $(ls ./examples/solvable/*);
 	do
+		dry=""
+
 		if [[ $file = *5* ]]
 		then
 			continue
@@ -40,8 +42,17 @@ do
 			continue
 		fi
 
+		if [[ $file = */*solvable-*.txt ]]
+		then
+			dry="--dry"
+			printf "--dry "
+		else
+			printf "      "
+		fi
+
 		printf "%s | %-40s " $command $file
-		RESULT=$(./target/release/n-puzzle $command $file > /dev/null 2> /dev/null)
+		RESULT=$(./target/release/n-puzzle $command $dry $file > /dev/null 2> /dev/null)
+
 		if [ $? -eq 0 ]
 		then
 			echo "\t[OK]"
